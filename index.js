20f1568500fb6bf758a3d0e4c53ab937c809226d
Abel Alem <81484647+abelalem123@users.noreply.github.com>
2022-11-29 10:12:21 +0300

    frontend added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 
 let notes = [
     {
@@ -78,7 +79,7 @@ app.get('/api/notes/:id', (request, response) => {
   
     response.json(note)
   })
-const Port=3001
+const Port=process.env.Port||3001
 app.listen(Port,()=>{
     console.log(`server is running on port ${Port}`);
 })
